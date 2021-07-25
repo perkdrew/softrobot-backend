01905ab7a78607163a4ccf9ac19830e801590c13
drewmaniva <drew@manivadigital.com>
2021-07-26 00:42:49 +0200

    refactoring sessions

--- src/handler.py
+++ src/handler.py
@@ -3,47 +3,53 @@ from urllib import parse
 import functools, logging, json
 from datetime import datetime
 
-from .utils.config import Session, cache
+from .utils.config import session
 from . import db
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path.endswith("/login"):
-            user_id = int(self.path.split("/")[1])
-            session = Session(user_id)
-            if cache != {} and user_id == cache["session"]["user_id"]:
-                logging.info(f"The user id {user_id} already exists.")
-                session_key = session.get_session_key()
-            else:
-                session_key = session.create_session()
-            return self.write_response(data=str({"user": session_key}))
+            try:
+                user_id = int(self.path.split("/")[1])
+                if session.store != {} and user_id == session.store["user_id"]:
+                    logging.info(f"The user id {user_id} already exists.")
+                    session_key = session.get_session_key(user_id)
+                else:
+                    session_key = session.create_session(user_id)
+                return self.write_response(data=str({"user": session_key}))
+            except:
+                return self.send_error(code=400)
 
         if self.path.endswith("/highscorelist"):
-            level_id = int(self.path.split("/")[1])
-            queryset = db.get_high_score_list(level_id, db.store)
-            return self.write_response(data=queryset)
+            try:
+                level_id = int(self.path.split("/")[1])
+                queryset = db.get_high_score_list(level_id, db.store)
+                return self.write_response(data=queryset)
+            except:
+                return self.send_error(code=400)
 
     def do_POST(self):
-        url = parse.urlparse(self.path).query
-        session_key = parse.parse_qs(url)["sessionkey"].pop()
-        if cache != {} and cache["session"]["session_key"] == session_key:
-            session = cache["session"]
-            if datetime.utcnow() <= session["exp"]:
-                level_id = int(self.path.split("/")[1])
-                length = int(self.headers.get("Content-Length"))
-                body = json.loads(self.rfile.read(length))
-                logging.info(f"POST body: {body}")
-                response = db.add_scores(
-                    level_id, body["score"], session["user_id"], db.store
-                )
-                return self.write_response(response)
+        try:
+            url = parse.urlparse(self.path).query
+            session_key = parse.parse_qs(url)["sessionkey"].pop()
+            if session.store != {} and session.store["session_key"] == session_key:
+                if datetime.utcnow() <= session.store["exp"]:
+                    level_id = int(self.path.split("/")[1])
+                    length = int(self.headers.get("Content-Length"))
+                    body = json.loads(self.rfile.read(length))
+                    logging.info(f"POST body: {body}")
+                    db.add_scores(
+                        level_id, body["score"], session.store["user_id"], db.store
+                    )
+                else:
+                    return self.send_error(code=400, message="Token is expired or invalid.")
             else:
-                return self.send_error(code=400, message="Token is expired or invalid.")
-        else:
-            return self.send_error(code=400, message="Authentication required.")
+                return self.send_error(code=400, message="Authentication required.")
+        except:
+            return self.send_error(code=400)
 
-    @functools.lru_cache(maxsize=5)
+    @functools.lru_cache(maxsize=4)
     def write_response(self, data, status=200, content_type="application/json"):
         if isinstance(data, str):
             data = data.encode("utf-8")
--- src/utils/config.py
+++ src/utils/config.py
@@ -2,26 +2,25 @@ import random, string
 from datetime import timedelta, datetime
 
 
-cache = {}
+class SessionStore:
+    def __init__(self):
+        self.store = {}
 
-
-class Session:
-    def __init__(self, user_id: int):
-        self.user_id = user_id
-
-    def create_session(self, minutes: int = 10):
+    def create_session(self, user_id: int, minutes: int = 10):
         self.exp = datetime.utcnow() + timedelta(minutes=minutes)
         self.session_key = "".join(
             random.choices(string.ascii_letters + string.digits, k=16)
         )
-        self.session = {
-            "user_id": self.user_id,
+        self.store = {
+            "user_id": user_id,
             "session_key": self.session_key,
             "exp": self.exp,
         }
-        cache["session"] = self.session
         return self.session_key
 
-    def get_session_key(self):
-        if cache["session"]["user_id"] == self.user_id:
-            return cache["session"]["session_key"]
+    def get_session_key(self, user_id: int):
+        if self.store["user_id"] == user_id:
+            return self.store["session_key"]
+
+
+session = SessionStore()
